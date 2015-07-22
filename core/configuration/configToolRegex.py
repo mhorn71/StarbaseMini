74c539818736d33a6aab7886f73782cdfa283490
Mark <mhorn71@gmail.com>
2015-07-22 23:33:20 +0100

    Add GPLv2 Preamble to files.

--- core/configuration/configToolRegex.py
+++ core/configuration/configToolRegex.py
@@ -1,4 +1,22 @@
 __author__ = 'mark'
+# StarbaseMini Staribus/Starinet Client for the British Astronomical Association Staribus Protocol
+# Copyright (C) 2015  Mark Horn
+#
+# This file is part of StarbaseMini.
+# 
+# StarbaseMini is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# StarbaseMini is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with StarbaseMini.  If not, see <http://www.gnu.org/licenses/>.
+
 '''
 Regex for configTool text line edits.
 
