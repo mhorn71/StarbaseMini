71b60c57dd04d46550668f9d3429b499cd56dd89
Mark Horn <mhorn71@gmail.com>
2015-07-28 23:26:15 +0100

    Moved ui from core to it's own separate module

--- core/__init__.py
+++ core/__init__.py
@@ -3,7 +3,5 @@ from core.configuration.configuration_loader import ConfigLoader
 from core.configuration.configuration_tool import ConfigManager
 from core.futurlec.baudrate import Baudrate
 from core.instument_builder.builder import InstrumentBuilder
-from core.ui.mainwindow import Ui_MainWindow
 from core.xml.instruments_loader import Instruments
 from core.xml.instrument_loader import Instrument
-from core.utilities.staribus_port_detect import StaribusPortDetect
