b9c87a35d0164f12b161e7f890a3d50467d028b1
Mark <mhorn71@gmail.com>
2015-08-03 15:19:12 +0100

    Updated core.futurlec module temp just to get UI going again.

--- core/futurlec/baudrate.py
+++ core/futurlec/baudrate.py
@@ -22,12 +22,12 @@ import logging
 
 from PyQt4 import QtGui
 
-from ui import Ui_Dialog
+from ui import Ui_FuturlecDialog
 
 logger = logging.getLogger('core.configTool')
 
 
-class Baudrate(QtGui.QDialog, Ui_Dialog):
+class Baudrate(QtGui.QDialog, Ui_FuturlecDialog):
     def __init__(self):
         QtGui.QDialog.__init__(self)
         self.setupUi(self)
