f450baca91c1f7a5c0a509d2b64909b3c4528440
Mark <mhorn71@gmail.com>
2015-08-03 14:36:33 +0100

    Updated config_utilities __init__.py

--- config_utilities/__init__.py
+++ config_utilities/__init__.py
@@ -2,4 +2,5 @@ __author__ = 'mark'
 
 from config_utilities.configuration_loader import ConfigTool
 from config_utilities.configuration_regex import *
+from config_utilities.configuration_tool import ConfigManager
 
