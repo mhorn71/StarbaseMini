8172dcb5d79132eed472dca41d5b920285302af1
Mark <mhorn71@gmail.com>
2015-08-27 23:44:23 +0100

    Removed charting legend use was inconsistent between platforms moved to colour indicators next to chart control buttons.

--- charting/charting.py
+++ charting/charting.py
@@ -175,10 +175,10 @@ class Chart:
             # todo add logger bits
             return 'PREMATURE_TERMINATION', str(msg)
 
-        self.ax1f1.xaxis.labelpad = 0.9
+        # self.ax1f1.xaxis.labelpad = 0.9
 
-        self.ax1f1.legend(loc='upper center', prop={'size': 10}, bbox_to_anchor=(0.5, -0.06), fancybox=True, shadow=False,
-                          ncol=number_of_channels)
+        # self.ax1f1.legend(loc='upper center', prop={'size': 10}, bbox_to_anchor=(0.5, -0.06), fancybox=True, shadow=False,
+        #                   ncol=number_of_channels)
 
         self.add_mpl()
 
