b5a90940b0fc05aa00af90ecaf1ea1456e283995
Mark <mhorn71@gmail.com>
2015-09-04 22:39:25 +0100

    Removed unwanted import of sys, added skeleton segment time series

--- config_utilities/configuration_loader.py
+++ config_utilities/configuration_loader.py
@@ -17,7 +17,6 @@ __author__ = 'mark'
 # You should have received a copy of the GNU General Public License
 # along with StarbaseMini.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
 import configparser
 from os import path, makedirs
 
