8b017ef986d65208b169e876b8213127062ca145
Mark <mhorn71@gmail.com>
2015-09-09 09:44:01 +0100

    added instruments attrib skeleton modules

---
+++ instrument_attrib/attributes.py
@@ -0,0 +1,50 @@
+__author__ = 'mark'
+# StarbaseMini Staribus/Starinet Client for the British Astronomical Association Staribus Protocol
+# Copyright (C) 2015  Mark Horn
+#
+# This file is part of StarbaseMini.
+#
+# StarbaseMini is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# StarbaseMini is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with StarbaseMini.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import logging
+
+from PyQt4 import QtGui
+
+from ui import Ui_InstrumentAttributesDialog
+
+logger = logging.getLogger('instrument.builder')
+
+
+class InstrumentAttrib(QtGui.QDialog,Ui_InstrumentAttributesDialog):
+    def __init__(self):
+        QtGui.QDialog.__init__(self)
+        self.setupUi(self)
+
+        # Style sheets
+        stylebool = False
+
+        if sys.platform.startswith('darwin'):
+            stylesheet = 'css/macStyle.css'
+            stylebool = True
+        elif sys.platform.startswith('win32'):
+            stylesheet = 'css/winStyle.css'
+            stylebool = True
+        elif sys.platform.startswith('linux'):
+            stylesheet = 'css/nixStyle.css'
+            stylebool = True
+
+        if stylebool:
+            with open(stylesheet, 'r') as style:
+                self.setStyleSheet(style.read())
