d99f5e7ef35005969e4ece6f5a77713da8bc371f
Mark <mhorn71@gmail.com>
2016-06-03 23:08:06 +0100

    Code tidy and add more loggers.

--- dao/staribus_response_message.py
+++ dao/staribus_response_message.py
@@ -56,71 +56,97 @@ import constants
 
 class StaribusResponseMessage:
     def __init__(self):
-        self.logger = logging.getLogger('dao.StaribusResponsMessage')
+        logger = logging.getLogger('dao.StaribusResponseMessage.init')
+        logger.debug('StaribusResponseMessage initialised.')
 
     def decipher(self, message):
 
+        logger = logging.getLogger('dao.StaribusResponseMessage.decipher')
+
         # strip STX from message
         stx_stripped = message.strip(constants.STX)
-        self.logger.debug('Message stripped of STX : %s' % repr(stx_stripped))
+
+        logger.debug('Message stripped of STX : %s' % repr(stx_stripped))
 
         # strip EOT CR LF
         to_strip = constants.EOT + constants.CR_LF
+
         message = stx_stripped.strip(to_strip)
-        self.logger.debug('Message stripped of EOT CR LF : %s' % repr(message))
+
+        logger.debug('Message stripped of EOT CR LF : %s' % repr(message))
 
         if len(message) == 18:
-            self.logger.debug('Message has no data payload.')
+
+            logger.debug('Message has no data payload.')
+
             if utilities.check_crc(message):
-                self.logger.debug('Message passed crc check.')
+
+                logger.debug('Message passed crc check.')
 
                 # Slice off the CRC code.
                 message_minus_crc = message[:14]
-                self.logger.debug('Message minus crc : %s' % message_minus_crc)
+
+                logger.debug('Message minus crc : %s' % message_minus_crc)
 
                 # remove the being of the message to get at status code.
                 status_code = message_minus_crc[-4:]
-                self.logger.debug('Message status byte code : %s' % status_code)
+
+                logger.debug('Message status byte code : %s' % status_code)
 
                 # convert the status code bytes to string.
                 status = utilities.staribus_status_b2str(status_code)
-                self.logger.debug('Message status code string : %s' % status)
+
+                logger.debug('Message status code string : %s' % status)
 
                 return status, None
 
             else:
-                self.logger.debug('Message failed crc check.')
+
+                logger.debug('Message failed crc check.')
+
                 return 'CRC_ERROR', None
 
         else:
-            self.logger.debug('Message has payload.')
+
+            logger.debug('Message has payload.')
+
             # as we have data we need to strip the last US from the message string before passing to check_crc.
             message = message.strip(constants.US)
-            self.logger.debug('Message stripped of last US : %s' % repr(message))
+
+            logger.debug('Message stripped of last US : %s' % repr(message))
 
             if utilities.check_crc(message):
-                self.logger.debug('Message passed crc check.')
+
+                logger.debug('Message passed crc check.')
+
                 # strip data payload from message so we can get at status code.
                 message_less_data = message[0:14]
-                self.logger.debug('Message striped of payload data : %s' % repr(message_less_data))
+
+                logger.debug('Message striped of payload data : %s' % repr(message_less_data))
 
                 # remove the being of the message to get at status code.
                 status_code = message_less_data[-4:]
-                self.logger.debug('Message status byte code : %s' % status_code)
+
+                logger.debug('Message status byte code : %s' % status_code)
 
                 # convert the status code bytes to string.
                 status = utilities.staribus_status_b2str(status_code)
-                self.logger.debug('Message status code string : %s' % status)
+
+                logger.debug('Message status code string : %s' % status)
 
                 # strip unwanted data from payload.
                 payload = message[14:-4]
-                self.logger.debug('Message payload : %s' % repr(payload))
+
+                logger.debug('Message payload : %s' % repr(payload))
 
                 # strip US from message.
                 payload = payload.replace(constants.US, '')
 
                 return status, payload
+
             else:
-                self.logger.debug('Message failed crc check.')
+
+                logger.debug('Message failed crc check.')
+
                 return 'CRC_ERROR', None
 
