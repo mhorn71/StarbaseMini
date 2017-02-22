31e4e20abf70f4d20fb3e28e51231f70b5f3844e
Mark <mhorn71@gmail.com>
2017-02-22 22:09:20 +0000

    Added Running Average UI files.

---
+++ ui/RunningAverage.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'RunningAverage.ui'
+#
+# Created by: PyQt5 UI code generator 5.4.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_RunningAverageDialog(object):
+    def setupUi(self, RunningAverageDialog):
+        RunningAverageDialog.setObjectName("RunningAverageDialog")
+        RunningAverageDialog.resize(205, 84)
+        self.gridLayout = QtWidgets.QGridLayout(RunningAverageDialog)
+        self.gridLayout.setObjectName("gridLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(RunningAverageDialog)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.spinBox = QtWidgets.QSpinBox(RunningAverageDialog)
+        self.spinBox.setObjectName("spinBox")
+        self.horizontalLayout.addWidget(self.spinBox)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+        self.buttonBox = QtWidgets.QDialogButtonBox(RunningAverageDialog)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Apply|QtWidgets.QDialogButtonBox.Cancel)
+        self.buttonBox.setCenterButtons(True)
+        self.buttonBox.setObjectName("buttonBox")
+        self.gridLayout.addWidget(self.buttonBox, 1, 0, 1, 1)
+
+        self.retranslateUi(RunningAverageDialog)
+        self.buttonBox.accepted.connect(RunningAverageDialog.accept)
+        self.buttonBox.rejected.connect(RunningAverageDialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(RunningAverageDialog)
+
+    def retranslateUi(self, RunningAverageDialog):
+        _translate = QtCore.QCoreApplication.translate
+        RunningAverageDialog.setWindowTitle(_translate("RunningAverageDialog", "Running Average"))
+        self.label.setText(_translate("RunningAverageDialog", "Average over n groups"))
+
--- ui/__init__.py
+++ ui/__init__.py
@@ -5,3 +5,4 @@ from ui.instrumentAttributes import Ui_InstrumentAttributesDialog
 from ui.releasenotes import Ui_Dialog
 from ui.metadata import Ui_MetadataDialog
 from ui.dataViewer import Ui_DataViewerDialog
+from ui.RunningAverage import Ui_RunningAverageDialog
